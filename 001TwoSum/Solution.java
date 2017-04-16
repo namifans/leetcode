504d7328b7cdf2a7eda51b3b97272c0360f9a6f1
namifans <yanglin0617@gmail.com>
2017-04-16 02:07:28 -0700

    Minor fix

--- 001TwoSum/Solution.java
+++ 001TwoSum/Solution.java
@@ -3,7 +3,7 @@ public class Solution {
 		int[] result = new int[2];
 		Map<Integer, Integer> map = new HashMap<>();
 		for (int i=0; i<nums.length; i++) {
-			if (map.containsKey(nums[i]) {
+			if (map.containsKey(nums[i])) {
 				result[0] = map.get(nums[i]);
 				result[1] = i;
 				return result;
