d00e92d53bcdb53ca7dcce3324f13fd59d65202d
namifans <yanglin0617@gmail.com>
2017-04-17 22:20:20 -0700

    028 Implement strstr()

---
+++ 028ImplementStrStr/Solution.java
@@ -0,0 +1,14 @@
+public class Solution {
+	public int strStr(String haystack, String needle) {
+		for (int i=0; ; i++) {
+			for (int j=0; ; j++) {
+				if (j == needle.length())
+					return i;
+				if (j + i == haystack.length())
+					return -1;
+				if (haystack.charAt(i+j) != needle.charAt(j))
+					break;
+			}
+		}
+	}
+}
