7dbb96e3a8cf065fceb6c283f68872f4d04c5779
namifans <yanglin0617@gmail.com>
2017-04-22 00:33:27 -0700

    243 Shortest word distance

---
+++ 243ShortestWordDistance/Solution.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public int shortestDistance(String[] words, String word1, String word2) {
+        int w1 = -1, w2 = -1, dis = Integer.MAX_VALUE;
+        for (int i = 0; i<words.length; i++) {
+        	if (words[i].equals(word1))
+        		w1 = i;
+        	if (words[i].equals(word2))
+        		w2 = i;
+        	dis = Math.min(dis, Math.abs(w2 - w1));
+        }
+        return dis;
+    }
+}
