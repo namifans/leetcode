016b89db3eb709a936376ec97f01bb284113081a
namifans <yanglin0617@gmail.com>
2017-04-27 00:53:10 -0700

    461 Hamming distance

---
+++ 461HammingDistance/Solution.java
@@ -0,0 +1,12 @@
+public class Solution {
+	public int hammingDistance(int x, int y) {
+		int xor = x ^ y;
+		int count = 0;
+		while (xor != 0) {
+			if ((xor >> 0 & 1) == 1)
+				count ++;
+			xor >>>= 1;
+		}
+		return count;
+	}
+}
