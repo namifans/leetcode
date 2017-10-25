45d8471560ab0769d215a4704506795719d22d9e
namifans <yanglin0617@gmail.com>
2017-10-24 23:53:33 -0700

    Minor update

--- alpha/001TwoSum/Solution.java
+++ alpha/001TwoSum/Solution.java
@@ -1,15 +1,15 @@
 public class Solution {
-	public int[] twoSum(int[] nums, int target) {
-		int[] result = new int[2];
-		Map<Integer, Integer> map = new HashMap<>();
-		for (int i=0; i<nums.length; i++) {
-			if (map.containsKey(nums[i])) {
-				result[0] = map.get(nums[i]);
-				result[1] = i;
-				return result;
-			}
-			map.put((target - nums[i]), i);
-		}
-		return result;
-	}
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i=0; i<nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                result[0] = map.get(nums[i]);
+                result[1] = i;
+                return result;
+            }
+            map.put((target - nums[i]), i);
+        }
+        return result;
+    }
 }
