2a4c47d55d3e38d086b184372e97e6ea55961686
namifans <yanglin0617@gmail.com>
2017-10-25 23:58:56 -0700

    Minor update

--- alpha/002AddTwoNumber/Solution.java
+++ alpha/002AddTwoNumber/Solution.java
@@ -1,31 +1,31 @@
 /**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
- * }
- */
+* Definition for singly-linked list.
+* public class ListNode {
+*     int val;
+*     ListNode next;
+*     ListNode(int x) { val = x; }
+* }
+*/
 public class Solution {
-	public ListNode addTwoNumbers(ListNode L1, ListNode L2) {
-		ListNode head = new ListNode(0);
-		ListNode current = head;
-		int carry = 0;
-		while (L1 != null || L2 != null || carry != 0) {
-			int temp = carry; 
-			if (L1 != null) {
-				temp += L1.val;
-				L1 = L1.next;
-			}
-			if (L2 != null) {
-				temp += L2.val;
-				L2 = L2.next;
-			}
-			carry = temp / 10;
-			ListNode node = new ListNode(temp % 10);
-			current.next = node;
-			current = current.next;
-		} 
-		return head.next;
-	}
+    public ListNode addTwoNumbers(ListNode L1, ListNode L2) {
+        ListNode head = new ListNode(0);
+        ListNode current = head;
+        int carry = 0;
+        while (L1 != null || L2 != null || carry != 0) {
+            int temp = carry; 
+            if (L1 != null) {
+                temp += L1.val;
+                L1 = L1.next;
+            }
+            if (L2 != null) {
+                temp += L2.val;
+                L2 = L2.next;
+            }
+            carry = temp / 10;
+            ListNode node = new ListNode(temp % 10);
+            current.next = node;
+            current = current.next;
+        } 
+        return head.next;
+    }
 }
