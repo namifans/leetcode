a962a6eb82dd525564218b2b459eb233c7a3ce66
namifans <yanglin0617@gmail.com>
2017-11-04 23:40:15 -0700

    Reformat

--- alpha/006ZigZagConversion/Solution.java
+++ alpha/006ZigZagConversion/Solution.java
@@ -1,28 +1,28 @@
 public class Solution {
     public String convert(String s, int numRows) {
-    	if (numRows <= 1)
-    		return s;
-    	char[] c = s.toCharArray();
+        if (numRows <= 1)
+            return s;
+        char[] c = s.toCharArray();
         StringBuilder[] sb = new StringBuilder[numRows];
         StringBuilder result = new StringBuilder();
         int index = 0;
         boolean down = true;
         for (int i = 0; i < numRows; i++) {
-    		sb[i] = new StringBuilder();
-		}
+            sb[i] = new StringBuilder();
+        }
         for (int i=0; i<s.length(); i++) {
-        	sb[index].append(c[i]);
-        	if (index == 0)
-        		down = true;
-        	if (index == numRows-1)
-        		down = false;
-        	if (down == true)
-        		index++;
-        	else 
-        		index--;
+            sb[index].append(c[i]);
+            if (index == 0)
+                down = true;
+            if (index == numRows-1)
+                down = false;
+            if (down == true)
+                index++;
+            else 
+                index--;
         }
         for (int i=0; i<numRows; i++) {
-        	result.append(sb[i].toString());
+            result.append(sb[i].toString());
         }
         return result.toString();
     }
