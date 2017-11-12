e2a9d1351b92f53df559193a92cf4cc331a10a9c
namifans <yanglin0617@gmail.com>
2017-11-11 16:21:19 -0800

    069 Sqrt(x)

---
+++ alpha/069Sqrt(x)/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int mySqrt(int x) {
+        if (x == 0)
+            return 0;
+        int left = 1, right = x;
+        int result = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (mid <= x / mid) {
+                left = mid + 1;
+                result = mid;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return result;       
+    }
+}
