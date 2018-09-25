6247e8747378fb04184ee4d1bb6427d36cd7c78c
namifans <yanglin0617@gmail.com>
2018-09-24 23:05:52 -0700

    001 Two Sum

---
+++ gamma/001TwoSum/Solutions.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        if (nums == null || nums.length < 2)
+            return result;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                result[0] = map.get(target - nums[i]);
+                result[1] = i;
+                return result;
+            } else {
+                map.put(nums[i], i);
+            } 
+        }
+        return result;
+    }
+}
