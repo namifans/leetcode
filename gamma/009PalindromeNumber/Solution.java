391bd0c9832578177b285fb4fb4472dc8b6c7326
namifans <yanglin0617@gmail.com>
2018-09-30 22:01:40 -0700

    009 Palindrome number

---
+++ gamma/009PalindromeNumber/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0)
+            return false;
+        int reverse = 0, temp = x;
+        while (temp != 0) {
+            reverse = reverse * 10 + temp % 10;
+            temp /= 10;
+        }
+        if (reverse == x)
+            return true;
+        return false;
+    }
+}
