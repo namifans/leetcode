df8e8f152c3279103d12a500f4a3cc32c2120f1f
namifans <yanglin0617@gmail.com>
2018-10-09 22:51:08 -0700

    028 Implement strStr()

---
+++ gamma/028ImplementStrStr/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        if (haystack == null || needle == null || haystack.length() < needle.length())
+            return -1;
+        if (haystack.isEmpty() && needle.isEmpty())
+            return 0;
+        for (int i = 0; i < haystack.length() - needle.length() + 1; i++) {
+            if (haystack.substring(i, i + needle.length()).equals(needle))
+                return i;
+        }
+        return -1;
+    }
+}
