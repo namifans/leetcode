cf73e1b1da13b3a43a1bae101398a80c89d19710
namifans <yanglin0617@gmail.com>
2018-10-27 22:03:54 -0700

    073 Set matrix zeroes

---
+++ gamma/073SetMatrixZeroes/Solution.java
@@ -0,0 +1,36 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        if (matrix == null || matrix.length == 0)
+            return;
+        int n = matrix.length, m = matrix[0].length;
+        boolean row = false, col = false;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if(matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                    if (i == 0)
+                        row = true;
+                    if (j == 0)
+                        col = true;
+                }
+            }
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0)
+                    matrix[i][j] = 0;
+            }
+        }
+        if (row) {
+            for (int j = 0; j < m; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+        if (col) {
+            for (int i = 0; i < n; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+}
