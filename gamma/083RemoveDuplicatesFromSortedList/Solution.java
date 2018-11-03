aaa0cca81560566f45f7f0bd3391968469ee2c3f
namifans <yanglin0617@gmail.com>
2018-11-02 22:43:40 -0700

    083 Remove duplicates from sorted list

---
+++ gamma/083RemoveDuplicatesFromSortedList/Solution.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+        ListNode current = head;
+        while (current != null && current.next != null) {
+            if (current.val != current.next.val) {
+                current = current.next;
+            } else {
+                current.next = current.next.next;
+            }
+        }
+        return head;
+    }
+}
