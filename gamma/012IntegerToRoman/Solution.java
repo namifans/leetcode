97dbeeb6c4b24df306b35b142371c6011351366e
namifans <yanglin0617@gmail.com>
2018-11-04 20:24:47 -0800

    Minor fix

--- gamma/012IntegerToRoman/Solution.java
+++ gamma/012IntegerToRoman/Solution.java
@@ -1,7 +1,7 @@
 class Solution {
     public String intToRoman(int num) {
         int[] nums = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-        String[] romans = {"M", "CM", "D", "CD", "C", "XC", "L", "LX", "X", "IX", "V", "IV", "I"};
+        String[] romans = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
         StringBuilder res = new StringBuilder();
         for (int i = 0; i < nums.length; i++) {
             if (num >= nums[i]) {
