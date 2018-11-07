1ace3221f016b58310fc1536aa418fcefbc703b1
namifans <yanglin0617@gmail.com>
2018-11-06 23:52:16 -0800

    155 Min stack

---
+++ gamma/155MinStack/Solution.java
@@ -0,0 +1,41 @@
+class MinStack {
+
+    Stack<Integer> minStack;
+    Stack<Integer> stack;
+    /** initialize your data structure here. */
+    public MinStack() {
+        minStack = new Stack<>();
+        stack = new Stack<>();
+    }
+    
+    public void push(int x) {
+        stack.push(x);
+        if (minStack.isEmpty() || x < minStack.peek()) {
+            minStack.push(x);
+        } else {
+            minStack.push(minStack.peek());
+        }
+    }
+    
+    public void pop() {
+        minStack.pop();
+        stack.pop();
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
