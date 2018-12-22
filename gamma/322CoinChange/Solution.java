8c1e94650fe7243e7406a28c517953939255b194
namifans <yanglin0617@gmail.com>
2018-12-21 23:48:59 -0800

    322 Coin change

---
+++ gamma/322CoinChange/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        if (amount < 1) {
+            return 0;
+        }
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0; j < coins.length; j++) {
+                if (i >= coins[j]) {
+                    int prev = dp[i - coins[j]];
+                    if (prev != Integer.MAX_VALUE)
+                        dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+                }
+            }
+        }
+        return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
+    }
+}
