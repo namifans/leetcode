b97531d26dbe81bc9674518421b4d41a101f2b31
namifans <yanglin0617@gmail.com>
2019-12-16 23:27:20 -0800

    141

---
+++ 2019/141LinkedListCycle/Solution.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+        ListNode s = head, f = head.next;
+        while (f != null && f.next != null) {
+            if (s == f) {
+                return true;
+            }
+            s = s.next;
+            f = f.next.next;
+        }
+        return false;
+    }
+}
