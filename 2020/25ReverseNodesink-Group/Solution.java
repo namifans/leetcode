ee15481b442cf227e533f709280422bd2ae461b3
namifans <yanglin0617@gmail.com>
2022-02-07 20:21:36 -0800

    old

---
+++ 2020/25ReverseNodesink-Group/Solution.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode dummy = new ListNode(0), slow = head, fast = head;
+        dummy.next = head;
+        int n = 0;
+        while (fast != null) {
+            fast = fast.next;
+            n++;
+        }
+        k = 
+    }
+}
--- 2020/692TopKFrequentWords/Solution.java
+++ 2020/692TopKFrequentWords/Solution.java
@@ -1,5 +1,19 @@
 class Solution {
     public List<String> topKFrequent(String[] words, int k) {
-        
+        Map<String, Integer> map = new HashMap<>();
+        for (String w : words) {
+            map.put(w, map.getOrDefault(w, 0) + 1);
+        }
+        Queue<Map.Entry<String, Integer>> heap = new PriorityQueue<>(
+            Comparator.comparing(Map.Entry<String, Integer>::getValue, Comparator.reverseOrder())
+            .thenComparing(Map.Entry<String, Integer>::getKey));
+        for (Map.Entry<String, Integer> e : map.entrySet()) {
+            heap.offer(e);
+        }
+        List<String> res = new ArrayList<>();
+        while (!heap.isEmpty() && res.size() < k) {
+            res.add(heap.poll().getKey());
+        }
+        return res;
     }
 }
