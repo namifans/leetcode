5591444a821fde22627123a5707fa3d7c6f57b80
namifans <yanglin0617@gmail.com>
2022-02-12 12:18:47 -0800

    103

---
+++ 2022/103. Binary Tree Zigzag Level Order Traversal/Solution.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        levelTraverse(root, 0, result);
+        return result;
+    }
+    
+    private void levelTraverse(TreeNode root, int depth, List<List<Integer>> result) {
+        if (root != null) {
+            List<Integer> list;
+            if (result.size() <= depth) {
+                list = new ArrayList<Integer>();
+                result.add(list);
+            } else {
+                list = result.get(depth);
+            }
+            if (depth % 2 == 0 ) {
+                list.add(root.val);
+            } else {
+                list.add(0, root.val);
+            }
+            levelTraverse(root.left, depth + 1, result);
+            levelTraverse(root.right, depth + 1, result);
+        }
+        
+    }
+}
