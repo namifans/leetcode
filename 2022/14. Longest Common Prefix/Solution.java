ddc4ec8bfd39889c39a7e0991f0725de4bdd4bd7
namifans <yanglin0617@gmail.com>
2022-02-16 17:38:58 -0800

    14

---
+++ 2022/14. Longest Common Prefix/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0)
+            return "";
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < strs[0].length(); i++) {
+            char c = strs[0].charAt(i);
+            for (int j = 1; j < strs.length; j++) {
+                if (strs[j].length() <= i || strs[j].charAt(i) != c) 
+                    return sb.toString();
+            }
+            sb.append(c);
+        }
+        return sb.toString();
+    }
+}
