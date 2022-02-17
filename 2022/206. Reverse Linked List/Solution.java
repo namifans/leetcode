d4727eef45139a7b2f9868f2e0e3ae43a5153463
namifans <yanglin0617@gmail.com>
2022-02-16 17:39:05 -0800

    206

---
+++ 2022/206. Reverse Linked List/Solution.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) 
+            return head;
+        ListNode dummy = new ListNode();
+        while (head != null) {
+            ListNode tmp = head.next;
+            head.next = dummy.next;
+            dummy.next = head;
+            head = tmp;
+        }
+        return dummy.next;
+    }
+}
