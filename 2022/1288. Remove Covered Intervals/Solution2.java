9e082f8a54f1a0e0dbe8d35c8dd18db4c783d356
namifans <yanglin0617@gmail.com>
2022-02-20 01:13:52 -0800

    1288

--- 2022/1288. Remove Covered Intervals/Solution2.java
+++ 2022/1288. Remove Covered Intervals/Solution2.java
@@ -1,10 +1,7 @@
 class Solution {
     public int removeCoveredIntervals(int[][] intervals) {
-        if (intervals.length <= 1) {
-            return intervals.length;    
-        }
         Arrays.sort(intervals, (x, y) ->  x[0] == y[0] ? y[1] - x[1] : x[0] - y[0]);
-        int i = 0, j = 1, count = 1;
+        int i = 0, j = 0, count = 1;
         while(j < intervals.length) {
             if (intervals[j][0] < intervals[i][0] || intervals[j][1] > intervals[i][1]) {
                 i = j;
