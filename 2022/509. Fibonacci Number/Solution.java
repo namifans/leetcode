44380b6ddb290f65c15636fc1ea4af1a7b6ff2aa
namifans <yanglin0617@gmail.com>
2022-02-20 23:35:50 -0800

    509

--- 2022/509. Fibonacci Number/Solution.java
+++ 2022/509. Fibonacci Number/Solution.java
@@ -1,8 +1,6 @@
 class Solution {
     public int fib(int n) {
-        if (n < 1)
-            return 0;
-        int[] f = new int[n + 1];
+        int[] f = new int[n + 2];
         f[0] = 0;
         f[1] = 1;
         for (int i = 2; i <= n; i++) {
