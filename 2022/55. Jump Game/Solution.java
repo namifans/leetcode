85922e08b64e8320c15cf93c2d9b3c75efeb6274
namifans <yanglin0617@gmail.com>
2022-02-23 22:50:08 -0800

    55

--- 2022/55. Jump Game/Solution.java
+++ 2022/55. Jump Game/Solution.java
@@ -3,13 +3,13 @@ class Solution {
         if (nums == null || nums.length == 0) {
             return false;
         }
-        int[] jump = new int[nums.length];
-        jump[0] = nums[0];
+        int jump = nums[0];
         for (int i = 1; i < nums.length; i++) {
-            if (jump[i - 1] == 0)
+            if (jump == 0) {
                 return false;
-            jump[i] = Math.max(jump[i - 1] - 1, nums[i]);
+            }
+            jump = Math.max(jump - 1, nums[i]);
         }
-        return true;
+        return jump >= 0;
     }
 }
