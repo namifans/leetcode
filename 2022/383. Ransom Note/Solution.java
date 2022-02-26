7273311ea114d3f7fea544e4c083a9e558a2a5ef
namifans <yanglin0617@gmail.com>
2022-02-25 18:55:58 -0800

    383

---
+++ 2022/383. Ransom Note/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] map = new int[26];
+        for (char c : magazine.toCharArray()) {
+            map[c - 'a']++;
+        }
+        for (char c : ransomNote.toCharArray()) {
+            if (map[c - 'a']-- == 0)
+                return false;
+        }
+        return true;
+    }
+}
