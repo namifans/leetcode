2c4f63ec33634e0a20752c27b29c254faa6cd9f9
namifans <yanglin0617@gmail.com>
2022-03-01 23:59:30 -0800

    75

---
+++ 2022/75. Sort Colors/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int i = 0, j = 0, k = nums.length - 1;
+        while (j <= k) {
+            if (nums[j] == 0) { 
+                swap(nums, i++, j);
+            } else if (nums[j] == 2) {
+                swap(nums, j--, k--);
+            }
+            j++;
+        }
+    }
+    
+    private void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+}
