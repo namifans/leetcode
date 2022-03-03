af3c6672cdf087091c920671d43e5adc3b779faa
namifans <yanglin0617@gmail.com>
2022-03-02 17:59:08 -0800

    101

---
+++ 2022/101. Symmetric Tree/Solution.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        return isSymmetric(root.left, root.right);
+    }
+    
+    private boolean isSymmetric(TreeNode left, TreeNode right) {
+        if (left == null && right == null)
+            return true;
+        else if (left == null || right == null)
+            return false;
+        else {
+            if (left.val != right.val)
+                return false;
+            return isSymmetric(left.right, right.left) && isSymmetric(left.left, right.right);
+        }
+    }
+}
