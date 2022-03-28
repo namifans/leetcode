0749226b8bb83f4ceb28aa61ef9cf2c2c24e287a
namifans <yanglin0617@gmail.com>
2022-03-27 22:41:31 -0700

    81

---
+++ 2022/81. Search in Rotated Sorted Array II/Solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean search(int[] nums, int target) {
+        int l = 0, r = nums.length - 1;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if (nums[m] == target) {
+                return true;
+            }
+            if (nums[l] < nums[m]) {
+                if (target > nums[m] || target < nums[l]) {
+                    l = m + 1;
+                } else {
+                    r = m - 1;
+                }
+            } else if (nums[l] > nums[m]) {
+                if (target < nums[m] || target > nums[r]) {
+                    r = m - 1;
+                } else {
+                    l = m + 1;
+                }
+            } else {
+                l++;
+            }
+        }
+        return false;
+    }
+}
