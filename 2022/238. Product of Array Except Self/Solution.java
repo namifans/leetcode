052e419c461ed0b0efc4d2df2af66912774eb085
namifans <yanglin0617@gmail.com>
2022-04-04 22:02:31 -0700

    238

---
+++ 2022/238. Product of Array Except Self/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int[] left = new int[nums.length], right = new int[nums.length];
+        left[0] = nums[0];
+        right[nums.length - 1] = nums[nums.length - 1];
+        for (int i = 1; i < nums.length; i++) {
+            left[i] = left[i - 1] * nums[i];
+        }
+        for (int i = nums.length - 2; i >= 0; i--) {
+            right[i] = right[i + 1] * nums[i];
+        }
+        int[] res = new int[nums.length];
+        res[0] = right[1];
+        res[nums.length - 1] = left[nums.length - 2];
+        for (int i = 1; i < nums.length - 1; i++) {
+            res[i] = left[i - 1] * right[i + 1];
+        }
+        return res;
+    }
+}
