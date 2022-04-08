bd7c9e6bd9666021dda34c2aac086eebe91f9d12
namifans <yanglin0617@gmail.com>
2022-04-07 22:48:30 -0700

    703

---
+++ 2022/703. Kth Largest Element in a Stream/Solution.java
@@ -0,0 +1,27 @@
+class KthLargest {
+    
+    private Queue<Integer> q;
+    private int k;
+
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        q = new PriorityQueue<>(k);
+        for (int n : nums) {
+            add(n);
+        }
+    }
+    
+    public int add(int val) {
+        q.add(val);
+        if (q.size() > k) {
+            q.remove();
+        }
+        return q.peek();
+    }
+}
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest obj = new KthLargest(k, nums);
+ * int param_1 = obj.add(val);
+ */
