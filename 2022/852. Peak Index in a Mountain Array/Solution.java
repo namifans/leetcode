cb23355afe0ccbf7245365e655ce8e68b2bf5cbe
namifans <yanglin0617@gmail.com>
2022-04-22 17:36:43 -0700

    Create Solution.java

---
+++ 2022/852. Peak Index in a Mountain Array/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int l = 0, r = arr.length - 1;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if (m == 0 || arr[m] > arr[m - 1]) {
+                l = m + 1;
+            } else {
+                r = m - 1;
+            }
+        }
+        return r;
+    }
+}
