080ed6a449cad4c473d247efc00aae20942210b7
namifans <yanglin0617@gmail.com>
2022-06-27 23:18:09 -0700

    1352

---
+++ 2022/1352. Product of the Last K Numbers/Solution.java
@@ -0,0 +1,32 @@
+class ProductOfNumbers {
+    
+    List<Integer> prefix;
+
+    public ProductOfNumbers() {
+        prefix = new ArrayList<>();
+        prefix.add(1);
+    }
+    
+    public void add(int num) {
+        if (num == 0) {
+            prefix = new ArrayList<>();
+            prefix.add(1);
+        } else {
+            prefix.add(prefix.get(prefix.size() - 1) * num);
+        }
+    }
+    
+    public int getProduct(int k) {
+        if (k >= prefix.size())
+            return 0;
+        else
+            return prefix.get(prefix.size() - 1) / prefix.get(prefix.size() - k - 1);
+    }
+}
+
+/**
+ * Your ProductOfNumbers object will be instantiated and called as such:
+ * ProductOfNumbers obj = new ProductOfNumbers();
+ * obj.add(num);
+ * int param_2 = obj.getProduct(k);
+ */
