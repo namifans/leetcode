3ffe683490e799ad987685a4243e17f6fada9ade
namifans <yanglin0617@gmail.com>
2024-03-14 19:42:23 -0700

    27

---
+++ 2024/27. Remove Element/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int i = 0, j = 0;
+        while (i <= j && j < nums.length) {
+            if (nums[j] != val) {
+                nums[i++] = nums[j++];
+            } else {
+                j++;
+            }
+        }
+        return i;
+    }
+}
