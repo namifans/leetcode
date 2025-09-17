bac724a087058e51fcb33eee51d2b0d5a1debe26
namifans <yanglin0617@gmail.com>
2025-09-16 21:29:14 -0700

    2

---
+++ 2025/2161. Partition Array According to Given Pivot/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
+        left, mid, right = [], [], []
+        for n in nums:
+            if n < pivot:
+                left.append(n)
+            elif n > pivot:
+                right.append(n)
+            else:
+                mid.append(n)
+        return left + mid + right
